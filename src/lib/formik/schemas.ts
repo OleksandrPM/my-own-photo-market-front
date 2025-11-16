a1521136641aea30aa077cb64c0527669146fd58
Oleksandr Moskaliuk <olekspm@gmail.com>
2025-11-16 19:39:14 +0100

    Create signIn & signUp schemas

---
+++ src/lib/formik/schemas.ts
@@ -0,0 +1,11 @@
+import { object, string } from "yup";
+
+export const signInSchema = object({
+  email: string().email("Invalid email addresss`").required("Required"),
+  password: string().min(8, "Must be 8 symbols or more").required("Required"),
+});
+
+export const signUpSchema = object({
+  email: string().email("Invalid email addresss`").required("Required"),
+  password: string().min(8, "Must be 8 symbols or more").required("Required"),
+});
