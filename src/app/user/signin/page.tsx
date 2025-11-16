c044bc70916f68c1b0374ceb19d77c465cb173da
Oleksandr Moskaliuk <olekspm@gmail.com>
2025-11-16 19:40:16 +0100

    Add form to the SignInPage

--- src/app/user/signin/page.tsx
+++ src/app/user/signin/page.tsx
@@ -1,7 +1,15 @@
+import SignInForm from "components/SignInForm";
+import Link from "next/link";
+
 export default function SignInPage() {
   return (
     <div className="container mx-auto">
       <h2 className="content-center">SignIn Page</h2>
+      <p>
+        Don't have an account yet?{" "}
+        <Link href="/user/signup">Create an account</Link>
+      </p>
+      <SignInForm />
     </div>
   );
 }
