de7f5ca3f06430c0621c68143cf380172b802add
Oleksandr Moskaliuk <olekspm@gmail.com>
2025-11-17 16:31:19 +0100

    Add isLoggedIn logic to the UserBar

--- src/components/UserBar/UserBar.tsx
+++ src/components/UserBar/UserBar.tsx
@@ -1,13 +1,29 @@
+"use client";
+
+import { useAppSelector } from "lib/hooks/react-redux-hooks";
+import {
+  getIsLoggedIn,
+  getUserEmail,
+  getUserName,
+} from "lib/redux/features/auth/authSelectors";
 import Link from "next/link";
 
 export default function UserBar() {
+  const isLoggedIn = useAppSelector(getIsLoggedIn);
+  const userName = useAppSelector(getUserName);
+  const email = useAppSelector(getUserEmail);
+
   return (
     <>
-      <p>
-        <Link href="/user/signup">Sign Up</Link>
-        <span> / </span>
-        <Link href="/user/signin">Sign in</Link>
-      </p>
+      {isLoggedIn ? (
+        <h4>{userName ? (userName as string) : (email as string)}</h4>
+      ) : (
+        <p>
+          <Link href="/user/signup">Sign Up</Link>
+          <span> / </span>
+          <Link href="/user/signin">Sign in</Link>
+        </p>
+      )}
     </>
   );
 }
