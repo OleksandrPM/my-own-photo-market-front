358aa688b286abd04d42964ebb7c562bd2e35adc
Oleksandr Moskaliuk <olekspm@gmail.com>
2025-12-12 20:40:26 +0100

    Separate searching logic at a separate page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,23 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 import ThemeSwitch from "components/ThemeSwitch";
-import Logo from "../Logo";
-import TagSearch from "../TagSearch";
-import UserBar from "../UserBar";
+import Logo from "components/Logo";
+import UserBar from "components/UserBar";
 
 export default function Header() {
+  const pathName = usePathname();
+
   return (
     <header>
       <div className="container mx-auto flex justify-between">
         <Logo />
-        <TagSearch />
+        {pathName !== "/search" ? (
+          <Link href="/search">Searching by tag</Link>
+        ) : (
+          <Link href="/">Back to the main page</Link>
+        )}
         <ThemeSwitch />
         <UserBar />
       </div>
--- src/components/TagSearch/TagSearch.tsx
+++ src/components/TagSearch/TagSearch.tsx
@@ -1,60 +1,130 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import clsx from "clsx";
-import { useAppSelector } from "lib/hooks/react-redux-hooks";
+import { useAppDispatch, useAppSelector } from "lib/hooks/react-redux-hooks";
 import { getTags } from "lib/redux/features/tags/tagsSelectors";
-import { searchImagesByTag } from "lib/api/imagesApi";
 
 import styles from "./TagSearch.module.scss";
+import { allTagsThunk } from "lib/redux/features/tags/tagsThunks";
+import { useRouter } from "next/navigation";
 
 export default function TagSearch() {
+  const dispatch = useAppDispatch();
   const tags = useAppSelector(getTags);
+  const [selectedTags, setSelectedTags] = useState([] as string[]);
   const [searchTerm, setSearchTerm] = useState("");
-  const [showSuggestions, setShowSuggestions] = useState(true);
+  const router = useRouter();
 
-  const filteredTags = tags.filter((tag) =>
-    tag.tag?.toLowerCase().includes(searchTerm.toLowerCase())
+  useEffect(() => {
+    dispatch(allTagsThunk());
+  }, [dispatch]);
+
+  const filteredTags = tags.filter(
+    (tag) =>
+      tag.tag?.toLowerCase().includes(searchTerm.toLowerCase()) &&
+      !selectedTags.includes(tag.tag)
   );
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(e.target.value);
   };
 
-  const handleTagClick = async (tagName: string) => {
-    setSearchTerm(tagName);
-    setShowSuggestions(false);
+  const handleSelect = async (tagName: string) => {
+    setSearchTerm("");
+    setSelectedTags((prevTags) => {
+      if (prevTags.includes(tagName)) {
+        return prevTags.filter((tag) => tag !== tagName);
+      } else {
+        return [...prevTags, tagName];
+      }
+    });
+  };
 
-    try {
-      // const data = searchImagesByTag(tagName);
-      // console.log("Backend response:", data);
-      // тут можна диспатчити в Redux або оновлювати локальний state
-    } catch (error) {
-      console.error("Error fetching data:", error);
+  const handleButtonClick = () => async () => {
+    if (selectedTags.length === 0) {
+      alert("No tag selected. View images without restrictions?");
+      return;
+    } else {
+      router.push(`/images/filtered?tags=${selectedTags.join(",")}`);
     }
   };
 
   return (
     <div className={clsx(styles.tagsSearchingInput)}>
+      <h3>
+        Select a tag from the list below or enter the tag name in the search
+        field
+      </h3>
+      {selectedTags.length > 0 && (
+        <p className={clsx(styles.selectedTags)}>
+          Selected tags: {selectedTags.join(", ")}
+        </p>
+      )}
       <input
+        id="tagSearch"
         type="text"
-        placeholder="Search by tag..."
+        placeholder="Search tag"
         value={searchTerm}
         onChange={handleChange}
       />
-      {searchTerm && showSuggestions && filteredTags.length > 0 && (
+
+      {filteredTags.length > 0 && (
         <ul className={clsx(styles.tagsList)}>
           {filteredTags.map((tag) => (
             <li
               className={clsx(styles.tagItem)}
               key={tag.id}
-              onClick={handleTagClick.bind(null, tag.tag)}
+              // onClick={handleTagClick.bind(null, tag.tag)}
             >
+              <input
+                type="checkbox"
+                checked={selectedTags.includes(tag.tag)}
+                onChange={() => handleSelect(tag.tag)}
+                title={tag.tag}
+                // readOnly
+              />
               {tag.tag}
             </li>
           ))}
         </ul>
       )}
+
+      <button type="button" onClick={handleButtonClick()}>
+        Search images
+      </button>
     </div>
   );
 }
+
+// const TagSearchInput = () => {
+//   const dispatch = useDispatch();
+//   const tags = useSelector((state) => state.tags.all);
+//   const selectedTags = useSelector((state) => state.tags.selected);
+//   const [term, setTerm] = useState("");
+
+//   const filtered = tags.filter((t) => t.includes(term));
+
+//   const handleSelect = (tag: string) => {
+//     dispatch(addTag(tag));
+//     setTerm("");
+//   };
+
+//   return (
+//     <div>
+//       <input value={term} onChange={(e) => setTerm(e.target.value)} />
+//       <ul>
+//         {filtered.map((tag) => (
+//           <li key={tag}>
+//             <input
+//               type="checkbox"
+//               checked={selectedTags.includes(tag)}
+//               onChange={() => handleSelect(tag)}
+//             />
+//             {tag}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
