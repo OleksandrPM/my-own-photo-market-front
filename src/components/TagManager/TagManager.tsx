4a886be7a385f6a5595fbf702ff7a7b5e4ff779c
Oleksandr Moskaliuk <olekspm@gmail.com>
2026-01-10 09:02:14 +0100

    Optimize TagManager

--- src/components/TagManager/TagManager.tsx
+++ src/components/TagManager/TagManager.tsx
@@ -1,33 +1,33 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { clsx } from "clsx";
+import { useCallback, useEffect, useState } from "react";
+import debounce from "lodash/debounce";
 import { useAppDispatch, useAppSelector } from "lib/hooks/react-redux-hooks";
-import {
-  addSelectedTags,
-  setNewTags,
-  toggleSelectedTag,
-} from "lib/redux/features/tags/tagsSlice";
+import { addSelectedTags } from "lib/redux/features/tags/tagsSlice";
 import {
   getTagsIsLoading,
   getNewTags,
   getSelectedTags,
-  getTags,
   getRemainingTags,
+  getExistedTags,
 } from "lib/redux/features/tags/tagsSelectors";
 import { allTagsThunk } from "lib/redux/features/tags/tagsThunks";
+import { Tag } from "types/tag";
+import TagsCheckboxList from "components/TagsCheckboxList";
 
 export default function TagManager() {
   const dispatch = useAppDispatch();
-  const tags = useAppSelector(getTags);
   const selectedTags = useAppSelector(getSelectedTags);
-  const newTags = useAppSelector(getNewTags);
   const isLoading = useAppSelector(getTagsIsLoading);
   const remainingTags = useAppSelector(getRemainingTags).sort((a, b) =>
-    a.tag.localeCompare(b.tag)
+    a.localeCompare(b)
   );
+  const newTags = useAppSelector(getNewTags);
+  const existedTags = useAppSelector(getExistedTags);
+
   const [searchTerm, setSearchTerm] = useState("");
-  const [enteredTags, setEnteredTags] = useState<string[]>([]);
+  const [enteredTags, setEnteredTags] = useState<Tag["tag"][]>([]);
+  const [lastTag, setLastTag] = useState("");
   const [renderedTags, setRenderedTags] = useState(remainingTags);
 
   // Downloading tags from the server
@@ -35,141 +35,94 @@ export default function TagManager() {
     dispatch(allTagsThunk());
   }, [dispatch]);
 
-  //   Filter remaining tags during entering tags in the input field
+  // Update rendered tags based on search term and remaining tags
   useEffect(() => {
-    if (enteredTags.length > 0) {
-      const rendered = remainingTags.filter(
+    if (searchTerm === "") {
+      setRenderedTags(remainingTags);
+    } else if (lastTag === "") {
+      const filtered = remainingTags.filter(
         (tag) =>
-          !enteredTags.some((entered) =>
-            tag.tag.toLowerCase().includes(entered)
+          !enteredTags.some((enteredTag) =>
+            tag.toLowerCase().includes(enteredTag.toLowerCase())
           )
       );
-      setRenderedTags(rendered);
+      setRenderedTags(filtered);
     } else {
-      setRenderedTags(remainingTags);
-    }
-  }, [remainingTags, enteredTags]);
-
-  // Set newTags
-  useEffect(() => {
-    const newTags = selectedTags.filter(
-      (tag) => !tags.some((t) => t.tag == tag)
-    );
-
-    dispatch(setNewTags(newTags));
-  }, [selectedTags, tags]);
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const input = e.target.value;
-    let tagsInInput = [] as string[];
-
-    setSearchTerm(input);
-
-    if (input.at(-1) === ",") {
-      //   LowerCase - no, also change spaces between words in tags
-      tagsInInput = input
-        .split(",")
-        .map((tag) => tag.trim())
-        .filter((tag) => tag !== "");
-
-      setEnteredTags(tagsInInput);
-      setRenderedTags(
-        remainingTags.filter(
-          (tag) =>
-            !tagsInInput.some((t) =>
-              t.toLowerCase().includes(tag.tag.toLowerCase())
-            )
-        )
-      );
-    }
-
-    let lastTag;
-
-    if (input.includes(",")) {
-      lastTag = input.split(",").pop()?.trim();
-    } else {
-      lastTag = input.trim();
-    }
-
-    if (lastTag && lastTag.length > 0 && lastTag !== "") {
       const filtered = remainingTags.filter((tag) =>
-        tag.tag.toLowerCase().includes(lastTag)
+        tag.toLowerCase().includes(lastTag.toLowerCase())
       );
       setRenderedTags(filtered);
     }
+  }, [searchTerm, lastTag, remainingTags]);
+
+  const debouncedProcessInput = useCallback(
+    debounce((input: string) => {
+      const tagsInInput = input.split(",").map((tag) => tag.trim());
+      const lastTag = tagsInInput.at(-1) ?? "";
+
+      setEnteredTags(tagsInInput.filter(Boolean));
+      setLastTag(lastTag);
+    }, 300),
+    []
+  );
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setSearchTerm(value);
+    debouncedProcessInput(value);
   };
 
-  const handleAdd = () => {
-    if (searchTerm.trim() === "") return;
+  const addInputedTags = () => {
+    if (enteredTags.length === 0) {
+      alert("Please enter at least one tag.");
+      setSearchTerm("");
+      return;
+    }
 
-    const tagsToAdd = searchTerm
-      .split(",")
-      .map((tag) => tag.trim())
-      .filter(
-        (tag) =>
-          tag !== "" &&
-          !selectedTags.map((t) => t.toLowerCase()).includes(tag.toLowerCase())
-      );
+    const tagsToAdd = enteredTags.filter(
+      (tag) =>
+        !selectedTags.map((t) => t.toLowerCase()).includes(tag.toLowerCase())
+    );
 
     dispatch(addSelectedTags(tagsToAdd));
     setSearchTerm("");
   };
 
-  const handleSelect = (tagName: string) => {
-    dispatch(toggleSelectedTag(tagName));
-    setRenderedTags(remainingTags);
+  const handleEnterType = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      addInputedTags();
+    }
+  };
+
+  const handleAddClick = () => {
+    addInputedTags();
   };
 
   return (
     <div className="tagManager">
       {/* Loader */}
       {isLoading && <p>Loading tags...</p>}
+
       {/* Selected Tags Block */}
       {selectedTags.length > 0 && (
         <div className="mb-4">
-          <h4 className="font-medium mb-2">Selected Tags:</h4>
+          <h4 className="font-medium mb-2">Selected tags:</h4>
           <div className="flex gap-2">
-            <div>
-              <h5>Existed tags:</h5>
-              <ul className=" gap-2">
-                {selectedTags
-                  .filter((tag) => !newTags.some((t) => t === tag))
-                  .map((tag) => (
-                    <li className="" key={tag}>
-                      <input
-                        type="checkbox"
-                        checked={selectedTags.includes(tag)}
-                        onChange={() => handleSelect(tag)}
-                        title={tag}
-                      />
-                      {tag}
-                    </li>
-                  ))}
-              </ul>
-            </div>
-            <div>
-              <h5>New tags:</h5>
-              <ul className=" gap-2">
-                {newTags.map((tag) => (
-                  <li className="" key={tag}>
-                    <input
-                      type="checkbox"
-                      checked={selectedTags.includes(tag)}
-                      onChange={() => handleSelect(tag)}
-                      title={tag}
-                    />
-                    {tag}
-                  </li>
-                ))}
-              </ul>
-            </div>
+            <TagsCheckboxList
+              renderedTags={existedTags}
+              title="Existed tags:"
+            />
+            {newTags.length > 0 && (
+              <TagsCheckboxList renderedTags={newTags} title="New tags:" />
+            )}
           </div>
         </div>
       )}
 
       {/* Add Tag Input */}
       <label htmlFor="addPhotoForm-tags">
-        Enter tag`s name or select from the list below
+        Enter tags separated by commas, or select tags from the list below
       </label>
       <div className="relative w-full">
         <input
@@ -179,10 +132,11 @@ export default function TagManager() {
           placeholder="tag1, tag2, tag3"
           value={searchTerm}
           onChange={handleChange}
+          onKeyDown={handleEnterType}
         />
         <button
           type="button"
-          onClick={handleAdd}
+          onClick={handleAddClick}
           className="absolute right-2 top-1/2 -translate-y-1/2 px-3 py-1 bg-blue-600 text-white rounded-md text-sm"
         >
           Add
@@ -191,19 +145,7 @@ export default function TagManager() {
 
       {/* Remaining/filtered Tags List */}
       {renderedTags.length > 0 && (
-        <ul className={clsx()}>
-          {renderedTags.map((tag) => (
-            <li className={clsx()} key={tag.id}>
-              <input
-                type="checkbox"
-                checked={selectedTags.includes(tag.tag)}
-                onChange={() => handleSelect(tag.tag)}
-                title={tag.tag}
-              />
-              {tag.tag}
-            </li>
-          ))}
-        </ul>
+        <TagsCheckboxList renderedTags={renderedTags} title="Available tags:" />
       )}
     </div>
   );
