19eb1bab2eede6636ce045bf660c9ec3e540ddd3
Oleksandr Moskaliuk <olekspm@gmail.com>
2026-05-08 13:47:50 +0200

    Update search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,9 +1,24 @@
-import TagSearch from "components/TagSearch";
+"use client";
+
+import TagManager from "@/components/TagManager";
+import { useAppSelector } from "@/lib/hooks/react-redux-hooks";
+import { getNewTags } from "@/lib/redux/features/tags/tagsSelectors";
 
 export default function SearchPage() {
+  const newTags = useAppSelector(getNewTags);
+  const newTagsString = newTags.join(", ");
+
+  // TODO: Think, if to add the Search Button or if to activate searching during the changing of the selectedTags
+
   return (
     <div className="container mx-auto">
-      <TagSearch />
+      {newTags.length > 0 && (
+        <div className="search_alert-block">
+          <h4>Alert!</h4>
+          <p>{`These tags are new and will not be used in searching process: ${newTagsString}`}</p>
+        </div>
+      )}
+      <TagManager />
     </div>
   );
 }
